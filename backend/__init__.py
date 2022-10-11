2b44e5a579566b77a3c8d775bf73a4adbc44c0bb
BrianQu <qujianning0401@163.com>
2022-10-10 21:03:29 -0400

    memcache initialization

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,12 +1,42 @@
 from flask import Flask
+from backend import main
+from flask_apscheduler import APScheduler
 
-global memcache
+global memcache  # memcache
+global memcache_stat  # statistic of the memcache
+global memcache_config  # configuration of the memcache
 
 webapp = Flask(__name__)
-memcache = {}
 
-from backend import main
+memcache = {}  # memcache format:
+# {'key': {'file': encoded base64 file, 'size': file size in Byte, 'timestamp': timestamp}}
+memcache_stat = {}  # memcache_stat format:
+# {'key_count': total count of key in cache,
+#  'size_count': total count of file size,
+#  'request_count': total request count,
+#  'miss_count': total miss request count,
+#  'hit_count': total hit request count,
+#  'miss_rate': miss request rate,
+#  'hit_rate': hit request rate}
+memcache_config = {}  # memcache_config format
+# {'capacity': capacity of memcache in MB,
+#  'replace_policy': replacement policy of memcache}
 
+# initialize the memcache_stat
+memcache_stat['key_count'] = 0
+memcache_stat['size_count'] = 0
+memcache_stat['request_count'] = 0
+memcache_stat['miss_count'] = 0
+memcache_stat['hit_count'] = 0
+memcache_stat['miss_rate'] = 0
+memcache_stat['hit_rate'] = 0
 
+# initialize the memcache_config
+memcache_config['capacity'] = 10
+memcache_config['replace_policy'] = 'Random'
+
+scheduler = APScheduler()
+scheduler.init_app(webapp)
+scheduler.start()
 
 
---
+++ memcache/memcache.py
@@ -0,0 +1,7 @@
+def add_memcache(key, file):
+    """
+    To add key and value to memcache, value need to be encoded
+    :param key: str
+    :param file: str
+    :return: None
+    """

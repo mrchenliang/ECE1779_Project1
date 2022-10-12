9038973a6fb068a6aec11c5400aa77b38ab64534
mrchenliang <hi@chenliang.ca>
2022-10-11 20:32:40 -0400

    remove set_cache from memcache

--- memcache/main.py
+++ memcache/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, send_file, json, jsonify, g
 from memcache import webapp
 from memcache.response_helper import response_builder
-from backend.cache_helper import get_cache, set_cache
+from backend.cache_helper import get_cache
 
 @webapp.route('/clear_cache', methods = ['GET', 'POST'])
 # clears the memcache object
@@ -16,7 +16,6 @@ def refresh_configuration():
     if not cache_properties == None:
         max_capacity = cache_properties[1]
         replacement_policy = cache_properties[2]
-        set_cache(max_capacity, replacement_policy)
         # implement setting up new memcache
         return response_builder(True)
     return None
